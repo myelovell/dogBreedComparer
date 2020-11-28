3c6cc55e8a308ef42a4e7139b673455d797c290c
myelovell <myelovell@hotmail.com>
2020-11-28 01:28:55 +0100

    pls work

---
+++ Other/Dogell-Data/test.rb
@@ -0,0 +1,10 @@
+
+
+
+for page 1-32
+    for dog 1-20
+        if dog != exctinct
+            aquire value for keys: Breed	Group	Popularity Rank	Origin	Other names	Breed type	Breed recognition	Price	Size	Weight	Avg. Weight	Height	Avg. Height	Coat	Color	Grooming	Shedding Level	Temperament 	Intelligent Rank	Trainability	Playfulness	Sensitivity Level	Affection Level	Social Needs	Barking	Watchdog ability	Territorial	Biting Potential	Mouthiness	Impulse to Wander/Roam	Prey Drive	Apartment Friendly	Adaptability	Tolerates Being Left Alone	Fighting Dog	Stranger friendly	Child Friendly	Cat friendly	Dog Friendly	Office friendly	Senior Citizens Friendly	Pet Friendly	Good For First Time Owners	Service Dog	Therapy Dog	Detection Dog/Sniffer Dog	Search and Rescue Dog (SAR)	Boat Dog	Cart Puling/Drafting Dog	Health Issues	Health Problems	Life Expectancy	Hypoallergenic	Energy Level	Exercise Need	Sleeping Need	Avg. daily food consumtion	Weight Gain Potential	Weather And Climate	Stinkiness	Drooling tendency	Gestation Length	How often can the Breed have a litter	Litter Size
+        end
+    end  
+end
